90a4bb02e75a950f9e0cef57145de88c44c8d557
Johannes Lötzsch <git@johannesloetzsch.de>
2025-05-11 23:29:17 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -72,6 +72,47 @@
         '';
       };
 
+      genode_python = genodeEnv.mkDerivation {
+        name = "python.iso";
+
+        repos = [
+          ./.
+        ];
+
+        buildInputs = with genode-utils.packages.${system}; [
+          grub2
+          nova
+          jitterentropy libc
+        ] ++ (with pkgs; [
+          git
+          zip
+        ]);
+
+        buildPhase = ''
+          #USER=j03
+          #cd $GENODE_DIR/repos/gems/sculpt/depot/
+          #mkdir $USER
+          #cp ~/.gnupg/pubkey.asc USER/pubkey
+          #echo 'http://johannesloetzsch.de' > download
+          #cd -
+
+          [ -d $GENODE_DIR/repos/world ] || git clone https://github.com/genodelabs/genode-world.git $GENODE_DIR/repos/world
+          $GENODE_DIR/tool/ports/prepare_port python3
+          $GENODE_DIR/tool/depot/create $USER/pkg/x86_64/python3 UPDATE_VERSIONS=1  ## FORCE=1 REBUILD= -j4
+          ls $GENODE_DIR/depot/$USER/pkg/python3
+          $GENODE_DIR/tool/depot/publish $USER/pkg/x86_64/python3/2025-05-11 XZ_THREADS=4
+          #ls $GENODE_DIR/public/$USER
+
+          #vim $GENODE_DIR/depot/$USER/index/25.04
+          #$GENODE_DIR/tool/depot/publish $USER/index/25.04
+
+        '';
+
+        installPhase = ''
+          #cp var/run/python.iso $out
+        '';
+      };
+
       #default = genode_bash_hello;
       #genode-pkgs = genode-utils.packages.${system};
     };
